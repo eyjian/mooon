7c541d8ad733b01ca07b3040cd9db556013a0420
eyjian <eyjian@qq.com>
2014-12-16 16:43:00 +0800

    new

---
+++ common_library/include/util/string_formatter.h
@@ -0,0 +1,136 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: jian yi, eyjian@qq.com
+ */
+#ifndef MOOON_UTIL_STRING_FORMATTER_H
+#define MOOON_UTIL_STRING_FORMATTER_H
+#include "util/config.h"
+UTIL_NAMESPACE_BEGIN
+
+/***
+ * StringFormatter解决什么问题？看下列一段代码：
+class CException
+{
+public:
+    explicit CException(int errcode, const char* errmsg);
+};
+
+void foo()
+{
+    throw CException(20141216, "invalid param");
+}
+
+这里的 "invalid param" 不够具体，foo()的调用者看到的是非完整的信息
+，如果调用者会将errmsg记录到日志，由于信息有损，会增加定位问题的难度。
+
+进行如下改进：
+void foo()
+{
+    std::stringstream ss;
+    ss < "param[" << param_name << "] invalid: " << param_value;
+    throw CException(20141216, ss.str());
+}
+
+上面的代码解决了问题，但书写有些繁琐。借助StringFormatter，则变成：
+void foo()
+{
+    throw CException(20141216, StringFormatter("param[%s] invalid: %s", param_name, param_value));
+}
+
+当然，这种做法同printf()一样，存在类型安全问题。
+ */
+
+/**
+ * 将变参转换成一个对象，使用示例:
+void foo(const StringFormatter& formatter)
+{
+    printf("%s\n", formatter.c_str());
+}
+
+extern "C" int main()
+{
+    foo(StringFormatter("%s|%d", "abc", 123));
+    return 0;
+}
+*/
+class StringFormatter
+{
+public:
+    // 使用简单，固化buffer_size
+    explicit StringFormatter(const char* format, ...) throw (std::bad_alloc)
+        : _buffer_size(1024)
+    {
+        va_list ap;
+        va_start(ap, format);
+
+        init(format, ap);
+        va_end(ap);
+    }
+
+    // 增加灵活性，buffer_size由参数传入
+    explicit StringFormatter(size_t buffer_size, const char* format, ...) throw (std::bad_alloc)
+        : _buffer_size(buffer_size)
+    {
+        va_list ap;
+        va_start(ap, format);
+
+        init(format, ap);
+        va_end(ap);
+    }
+
+    size_t buffer_size() const throw ()
+    {
+        return _buffer_size;
+    }
+
+    // 仿std::string::c_str()命名
+    const char* c_str() const throw ()
+    {
+        return _buffer.c_str();
+    }
+
+    // 仿std::stringstream::str()命名
+    const std::string& str() const throw ()
+    {
+        return _buffer;
+    }
+
+private:
+    void init(const char* format, va_list& ap) throw (std::bad_alloc)
+    {        
+        char* buffer = new char[_buffer_size];
+        vsnprintf(buffer.get(), _buffer_size, format, ap);
+
+        // 保存结果
+        DeleteHelper<char> dh(buffer, true);
+        _buffer = buffer;
+    }
+
+private:
+    StringFormatter();
+    StringFormatter(const StringFormatter& );
+    StringFormatter& operator =(const StringFormatter&);
+    bool operator ==(const StringFormatter&) const;
+    bool operator !=(const StringFormatter&) const;
+
+private:
+    std::string _buffer;
+    size_t _buffer_size;
+};
+
+UTIL_NAMESPACE_END
+#endif // MOOON_UTIL_STRING_FORMATTER_H
---
+++ common_library/include/util/string_util.h
@@ -0,0 +1,260 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: jian yi, eyjian@qq.com
+ */
+#ifndef MOOON_UTIL_STRING_UTIL_H
+#define MOOON_UTIL_STRING_UTIL_H
+#include <math.h>
+#include <sstream>
+#include "util/config.h"
+UTIL_NAMESPACE_BEGIN
+
+class CStringUtil
+{
+public:
+	/** 删除字符串尾部从指定字符开始的内容
+	  * @source: 需要处理的字符串
+	  * @c: 分隔字符
+	  * @example:  如果str为“/usr/local/test/bin/”，而c为“/”，
+      *            则处理后str变成“/usr/local/test/bin”
+	  */
+	static void remove_last(std::string& source, char c);
+
+	/** 删除字符串尾部从指定字符串开始的内容
+	  * @source: 需要处理的字符串
+	  * @sep: 分隔字符串
+	  * @example: 如果str为“/usr/local/test/bin/tt”，而sep为“/bin/”，
+      *           则处理后str变成“/usr/local/test
+	  */
+	static void remove_last(std::string& source, const std::string& sep);
+
+    /** 将字符串中的所有小写字符转换成大写 */
+    static void to_upper(char* source);
+    static void to_upper(std::string& source);
+
+    /** 将字符串中的所有大写字符转换成小写 */
+    static void to_lower(char* source);    
+    static void to_lower(std::string& source);
+
+    /** 判断指定字符是否为空格或TAB符(\t)或回车符(\r)或换行符(\n) */
+    static bool is_space(char c);
+    
+    /** 删除字符串首尾空格或TAB符(\t)或回车符(\r)或换行符(\n) */
+    static void trim(char* source);
+    static void trim(std::string& source);
+
+    /** 删除字符串首部空格或TAB符(\t)或回车符(\r)或换行符(\n) */
+    static void trim_left(char* source);
+    static void trim_left(std::string& source);
+
+    /** 删除字符串尾部空格或TAB符(\t)或回车符(\r)或换行符(\n) */
+    static void trim_right(char* source);        
+    static void trim_right(std::string& source);
+
+	/**
+	  * 字符串转换成整数函数
+	  */
+	
+    /** 将字符串转换成8位的有符号整数
+	  * @source: 待转换成整数的字符串
+	  * @result: 转换后的整数
+      * @converted_length: 需要进行数字转换的字符个数，超过部分不做解析，如果取值为0则处理整个字符串
+      * @ignored_zero: 是否允许允许字符串以0打头，并自动忽略
+	  * @return: 如果转换成功返回true，否则返回false
+	  */
+    static bool string2int(const char* source, int8_t& result, uint8_t converted_length=0, bool ignored_zero=false);
+	static bool string2int8(const char* source, int8_t& result, uint8_t converted_length=0, bool ignored_zero=false);    
+
+	/** 将字符串转换成16位的有符号整数
+	  * @source: 待转换成整数的字符串
+	  * @result: 转换后的整数
+      * @converted_length: 需要进行数字转换的字符个数，超过部分不做解析，如果取值为0则处理整个字符串
+      * @ignored_zero: 是否允许允许字符串以0打头，并自动忽略
+	  * @return: 如果转换成功返回true，否则返回false
+	  */
+    static bool string2int(const char* source, int16_t& result, uint8_t converted_length=0, bool ignored_zero=false);
+	static bool string2int16(const char* source, int16_t& result, uint8_t converted_length=0, bool ignored_zero=false);    
+
+	/** 将字符串转换成32位的有符号整数
+	  * @source: 待转换成整数的字符串
+	  * @result: 转换后的整数
+      * @converted_length: 需要进行数字转换的字符个数，超过部分不做解析，如果取值为0则处理整个字符串
+      * @ignored_zero: 是否允许允许字符串以0打头，并自动忽略
+	  * @return: 如果转换成功返回true，否则返回false
+	  */
+    static bool string2int(const char* source, int32_t& result, uint8_t converted_length=0, bool ignored_zero=false);
+	static bool string2int32(const char* source, int32_t& result, uint8_t converted_length=0, bool ignored_zero=false);    
+
+	/** 将字符串转换成64位的有符号整数
+	  * @source: 待转换成整数的字符串
+	  * @result: 转换后的整数
+      * @converted_length: 需要进行数字转换的字符个数，超过部分不做解析，如果取值为0则处理整个字符串
+      * @ignored_zero: 是否允许允许字符串以0打头，并自动忽略
+	  * @return: 如果转换成功返回true，否则返回false
+	  */
+    static bool string2int(const char* source, int64_t& result, uint8_t converted_length=0, bool ignored_zero=false);
+	static bool string2int64(const char* source, int64_t& result, uint8_t converted_length=0, bool ignored_zero=false);    
+
+    /** 将字符串转换成8位的无符号整数
+	  * @source: 待转换成整数的字符串
+	  * @result: 转换后的整数
+      * @converted_length: 需要进行数字转换的字符个数，超过部分不做解析，如果取值为0则处理整个字符串
+      * @ignored_zero: 是否允许允许字符串以0打头，并自动忽略
+	  * @return: 如果转换成功返回true，否则返回false
+	  */
+    static bool string2int(const char* source, uint8_t& result, uint8_t converted_length=0, bool ignored_zero=false);
+	static bool string2uint8(const char* source, uint8_t& result, uint8_t converted_length=0, bool ignored_zero=false);    
+
+	/** 将字符串转换成16位的无符号整数
+	  * @source: 待转换成整数的字符串
+	  * @result: 转换后的整数
+      * @converted_length: 需要进行数字转换的字符个数，超过部分不做解析，如果取值为0则处理整个字符串
+      * @ignored_zero: 是否允许允许字符串以0打头，并自动忽略
+	  * @return: 如果转换成功返回true，否则返回false
+	  */
+    static bool string2int(const char* source, uint16_t& result, uint8_t converted_length=0, bool ignored_zero=false);
+	static bool string2uint16(const char* source, uint16_t& result, uint8_t converted_length=0, bool ignored_zero=false);    
+
+	/** 将字符串转换成32位的无符号整数
+	  * @source: 待转换成整数的字符串
+	  * @result: 转换后的整数
+      * @converted_length: 需要进行数字转换的字符个数，超过部分不做解析，如果取值为0则处理整个字符串
+      * @ignored_zero: 是否允许允许字符串以0打头，并自动忽略
+	  * @return: 如果转换成功返回true，否则返回false
+	  */
+    static bool string2int(const char* source, uint32_t& result, uint8_t converted_length=0, bool ignored_zero=false);
+	static bool string2uint32(const char* source, uint32_t& result, uint8_t converted_length=0, bool ignored_zero=false);    
+
+	/** 将字符串转换成64位的无符号整数
+	  * @source: 待转换成整数的字符串
+	  * @result: 转换后的整数
+      * @converted_length: 需要进行数字转换的字符个数，超过部分不做解析，如果取值为0则处理整个字符串
+      * @ignored_zero: 是否允许允许字符串以0打头，并自动忽略
+	  * @return: 如果转换成功返回true，否则返回false
+	  */
+    static bool string2int(const char* source, uint64_t& result, uint8_t converted_length=0, bool ignored_zero=false);
+	static bool string2uint64(const char* source, uint64_t& result, uint8_t converted_length=0, bool ignored_zero=false);    
+
+    static std::string int_tostring(int16_t source);
+    static std::string int16_tostring(int16_t source);    
+
+    static std::string int32_tostring(int32_t source);
+    static std::string int_tostring(int32_t source);
+
+    static std::string int_tostring(int64_t source);
+    static std::string int64_tostring(int64_t source);
+
+    static std::string int_tostring(uint16_t source);
+    static std::string uint16_tostring(uint16_t source);    
+
+    static std::string int_tostring(uint32_t source);
+    static std::string uint32_tostring(uint32_t source);    
+
+    static std::string int_tostring(uint64_t source);
+    static std::string uint64_tostring(uint64_t source);    
+    
+    /** 跳过空格部分
+      */
+    static char* skip_spaces(char* buffer);
+    static const char* skip_spaces(const char* buffer);
+
+    static uint32_t hash(const char *str, int len);
+
+    /***
+      * 不同于标准库的snprintf，这里的snprintf总是保证返回实际向str写入的字节数，
+      * 包括结尾符，而不管size是否足够容纳，其它行为相同
+      */
+    static int fix_snprintf(char *str, size_t size, const char *format, ...);
+    static int fix_vsnprintf(char *str, size_t size, const char *format, va_list ap);
+
+    /** 路径转换成文件名 */
+    static std::string path2filename(const std::string& path, const std::string& join_string);
+
+    /** 万用类型转换函数 */
+    template <typename Any>
+    std::string any2string(Any any)
+    {
+        std::stringstream s;
+        s << any;
+        return s.str();
+    }
+
+    /** 将STL容器转换成字符串 */
+    template <class ContainerClass>
+    static std::string container2string(const ContainerClass& container, const std::string& join_string)
+    {
+        std::string str;
+        typename ContainerClass::const_iterator iter = container.begin();
+
+        for (; iter!=container.end(); ++iter)
+        {
+            if (str.empty())
+                str = any2string(*iter);
+            else
+                str += join_string + any2string(*iter);
+        }
+
+        return str;
+    }
+    
+    /** 将map容器转换成字符串 */
+    template <class MapClass>
+    static std::string map2string(const MapClass& map, const std::string& join_string)
+    {
+        std::string str;
+        typename MapClass::const_iterator iter = map.begin();
+
+        for (; iter!=map.end(); ++iter)
+        {
+            if (str.empty())
+                str = any2string(iter->second);
+            else
+                str += join_string + any2string(iter->second);
+        }
+
+        return str;
+    }
+
+    /***
+      * 求得一个字符在字符串中的位置
+      * @return 如果c在字符串中，则返回非负整数值，否则返回-1
+      */
+    static int chr_index(const char* str, char c);
+    static int chr_rindex(const char* str, char c);
+    
+    /***
+      * 从文件路径中提取目录路径
+      * @return 返回提取到的目录路径
+      */
+    static std::string extract_dirpath(const char* filepath);
+    
+    /***
+      * 从文件路径中提取出文件名
+      * @return 返回提取到文件名，如果filepath是一个目录形式，则返回空串
+      */
+    static std::string extract_filename(const std::string& filepath);
+
+    /***
+      * 通过格式化生成一个字符串
+      * 请注意字符串的长度不能超过10240字节，否则会被截断
+      * @return 返回生成的字符串
+      */
+    static std::string format_string(const char* format, ...);
+};
+
+UTIL_NAMESPACE_END
+#endif // MOOON_UTIL_STRING_UTIL_H
