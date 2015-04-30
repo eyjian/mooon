e1466ff49b3e890421c7e9c37342da6d10e6c03e
eyjian <eyjian@live.com>
2015-04-30 19:24:46 +0800

    util->utils

--- common_library/test/plugin/plugin_mysql/run.sh
+++ common_library/test/plugin/plugin_mysql/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=.:../../../src/util:../../../src/net:../../../src/sys:../../../src/plugin/plugin_mysql:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=.:../../../src/utils:../../../src/net:../../../src/sys:../../../src/plugin/plugin_mysql:$LD_LIBRARY_PATH
 
 FILES=`ls ut_*|grep -v ".cpp"`
 for file in $FILES;
