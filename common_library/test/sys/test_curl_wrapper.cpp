1723c8e50f7980a0162f2b48f4a1cc49c19ff779
eyjian <eyjian@live.com>
2015-05-06 10:05:50 +0800

    fix Makefile

---
+++ common_library/test/sys/test_curl_wrapper.cpp
@@ -0,0 +1,47 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: eyjian@qq.com or eyjian@gmail.com or eyjian@live.com
+ */
+#include <mooon/sys/curl_wrapper.h>
+MOOON_NAMESPACE_USE
+
+extern "C" int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: curl_get url\n");
+        exit(1);
+    }
+    
+    try
+    {
+        sys::CCurlWrapper curl_wrapper(2);
+        std::string result;
+        curl_wrapper.get(&result, argv[1]);
+        printf("result =>\n%s\n", result.c_str());
+
+        result.clear();
+        curl_wrapper.get(&result, argv[1]);
+        printf("result =>\n%s\n", result.c_str());
+    }
+    catch (utils::CException& ex)
+    {
+        fprintf(stderr, "%s\n", ex.str().c_str());
+    }
+    
+    return 0;
+}
