8d3c06fe140f2d71c10dbd95758ca7b4112e6cab
eyjian <eyjian@live.com>
2015-06-08 16:54:49 +0800

    optimize

--- common_library/include/mooon/utils/array_queue.h
+++ common_library/include/mooon/utils/array_queue.h
@@ -33,10 +33,10 @@ public:
       * 构造一个数组队列
       * @queue_max: 需要构造的队列大小
       */
-	CArrayQueue(uint32_t queue_max)
-		:_tail(0)
-		,_head(0)
-		,_queue_size(0)
+    CArrayQueue(uint32_t queue_max)
+        :_tail(0)
+        ,_head(0)
+        ,_queue_size(0)
     {
         _queue_max = queue_max;
         if (0 == _queue_max)
@@ -45,54 +45,53 @@ public:
         }
         else
         {
-            _queue_max = _queue_max + 1;
             _elem_array = new DataType[_queue_max];        
             memset(_elem_array, 0, _queue_max);
         }
     }
-	
+
     ~CArrayQueue()
     {
         delete []_elem_array;
     }
-        
+
     /** 判断队列是否已满 */
     bool is_full() const 
-	{
-        return ((_tail+1) % _queue_max == _head);
+    {
+        return (_queue_max == _queue_size); //((_tail+1) % _queue_max == _head);
     }
     
     /** 判断队列是否为空 */
     bool is_empty() const 
-	{
-        return (_head == _tail);
+    {
+        return (0 == _queue_size); //(_head == _tail);
     }
 
     /** 返回队首元素 */
     DataType front() const 
-	{
+    {
         return _elem_array[_head];
     }
     
-	/***
+    /***
       * 弹出队首元素
       * 注意: 调用pop之前应当先使用is_empty判断一下
       * @return: 返回队首元素
       */
     DataType pop_front() 
-	{
+    {
         DataType elem = _elem_array[_head];
         _head = (_head+1) % _queue_max;
         --_queue_size;
         return elem;
     }
     
-	/***
+    /***
       * 往队尾插入一个元素
       * 注意: 调用pop之前应当先使用is_full判断一下
       */
     void push_back(DataType elem) 
-	{
+    {
         _elem_array[_tail] = elem;
         _tail = (_tail+1) % _queue_max; 
         ++_queue_size;
@@ -100,22 +99,22 @@ public:
 
     /** 得到队列中存储的元素个数 */
     uint32_t size() const 
-	{ 
-		return _queue_size; 
-	}
+    {
+        return _queue_size;
+    }
     
     /** 得到队列的容量 */
     uint32_t capacity() const
     {
-        return (0 == _queue_max)? 0: (_queue_max-1);
+        return _queue_max;
     }
-    
+
 private:        
     volatile uint32_t _tail;       /** 队首 */
     volatile uint32_t _head;       /** 队尾 */
     volatile uint32_t _queue_size; /** 队列当前容纳的元素个数 */
-	uint32_t _queue_max;  /** 队列最多可容纳的元素个数 */
-	DataType* _elem_array; /** 用来实现队列的数组 */
+    uint32_t _queue_max;  /** 队列最多可容纳的元素个数 */
+    DataType* _elem_array; /** 用来实现队列的数组 */
 };
 
 UTILS_NAMESPACE_END
