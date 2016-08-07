252c1d6025ccd095b76bf1d6eca6a9c34794ee0f
eyjian <eyjian@qq.com>
2016-08-07 11:19:33 +0800

    add linux include

---
+++ linux_include/linux/version.h
@@ -0,0 +1,7 @@
+#define LINUX_VERSION_CODE 199168
+#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
+#define RHEL_MAJOR 7
+#define RHEL_MINOR 0
+#define RHEL_RELEASE_VERSION(a,b) (((a) << 8) + (b))
+#define RHEL_RELEASE_CODE 1792
+#define RHEL_RELEASE "123.20.1"
