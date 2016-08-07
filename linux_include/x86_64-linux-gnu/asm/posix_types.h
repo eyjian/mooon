a3f7891e510c6b463a3ea1d3de02ce886a722ec1
eyjian <eyjian@qq.com>
2016-08-07 10:54:38 +0800

    更新linux头文件

--- linux_include/x86_64-linux-gnu/asm/posix_types.h
+++
@@ -1,5 +0,0 @@
-# ifdef __i386__
-#  include <asm/posix_types_32.h>
-# else
-#  include <asm/posix_types_64.h>
-# endif
--- linux_include/x86_64-linux-gnu/asm/unistd.h
+++
@@ -1,5 +0,0 @@
-# ifdef __i386__
-#  include <asm/unistd_32.h>
-# else
-#  include <asm/unistd_64.h>
-# endif
