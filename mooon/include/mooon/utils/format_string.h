4ad0090c99f42ae49988998f852bc7aaebda602e
eyjian <eyjian@live.com>
2016-09-19 11:27:03 +0800

    检查tokens个数和format格式是否正确，不做检查，当输入不正确的时候会abort

--- mooon/include/mooon/utils/format_string.h
+++ mooon/include/mooon/utils/format_string.h
@@ -9,6 +9,30 @@ UTILS_NAMESPACE_BEGIN
 enum { FORMAT_STRING_SIZE = 56 };
 inline std::string format_string(const char* format, const std::vector<std::string>& tokens)
 {
+    // 确保"%s"的个数为tokens.size()
+    std::vector<std::string>::size_type excepted_num_tokens = 0;
+    const char* format_p = format;
+    while (*format_p != '\0')
+    {
+        if (*format_p != '%')
+        {
+            ++format_p;
+        }
+        else
+        {
+            ++format_p;
+            if (('\0' == *format_p) || (*format_p != 's'))
+                return std::string(""); // error，只能为%s，不能为其它如%d等，也不能为单个的%
+
+            ++excepted_num_tokens;
+            ++format_p;
+        }
+    }
+    if (excepted_num_tokens != tokens.size())
+    {
+        return std::string("");
+    }
+
     if (0 == tokens.size())
     {
         return CStringUtils::format_string(format);
@@ -238,7 +262,7 @@ inline std::string format_string(const char* format, const std::vector<std::stri
         return CStringUtils::format_string(format, tokens[0].c_str(), tokens[1].c_str(), tokens[2].c_str(), tokens[3].c_str(), tokens[4].c_str(), tokens[5].c_str(), tokens[6].c_str(), tokens[7].c_str(), tokens[8].c_str(), tokens[9].c_str(), tokens[10].c_str(), tokens[11].c_str(), tokens[12].c_str(), tokens[13].c_str(), tokens[14].c_str(), tokens[15].c_str(), tokens[16].c_str(), tokens[17].c_str(), tokens[18].c_str(), tokens[19].c_str(), tokens[20].c_str(), tokens[21].c_str(), tokens[22].c_str(), tokens[23].c_str(), tokens[24].c_str(), tokens[25].c_str(), tokens[26].c_str(), tokens[27].c_str(), tokens[28].c_str(), tokens[29].c_str(), tokens[30].c_str(), tokens[31].c_str(), tokens[32].c_str(), tokens[33].c_str(), tokens[34].c_str(), tokens[35].c_str(), tokens[36].c_str(), tokens[37].c_str(), tokens[38].c_str(), tokens[39].c_str(), tokens[40].c_str(), tokens[41].c_str(), tokens[42].c_str(), tokens[43].c_str(), tokens[44].c_str(), tokens[45].c_str(), tokens[46].c_str(), tokens[47].c_str(), tokens[48].c_str(), tokens[49].c_str(), tokens[50].c_str(), tokens[51].c_str(), tokens[52].c_str(), tokens[53].c_str(), tokens[54].c_str(), tokens[55].c_str());
     }
 
-    MOOON_ASSERT(false);
+    //MOOON_ASSERT(false);
     return std::string("");
 }
 
