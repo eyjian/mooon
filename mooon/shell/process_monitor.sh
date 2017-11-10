c3117028960ed0e95ab9386d3fbd5a76effae5ee
eyjian <eyjian@live.com>
2017-11-10 14:07:24 +0800

    增强判断，要求目录部分完全相同

--- mooon/shell/process_monitor.sh
+++ mooon/shell/process_monitor.sh
@@ -2,39 +2,25 @@
 # https://github.com/eyjian/mooon/blob/master/common_library/shell/process_monitor.sh
 # Created by yijian on 2012/7/23
 #
-# 运行日志：/tmp/process_monitor.log，由于多进程同时写，不一定完整，仅供参考。
+# 运行日志：/tmp/process_monitor-USER.log，由于多进程同时写，不一定完整，仅供参考。
 # 请放到crontab中运行，如（注意要以后台方式运行，因为脚本是常驻不退出的）：
 # * * * * * /usr/local/bin/process_monitor.sh /usr/sbin/rinetd /usr/sbin/rinetd > /dev/null 2>&1 &
 #
 # 进程监控脚本，当指定进程不存在时，执行重启脚本将它拉起
+#
 # 特色：
-# 1.本监控脚本可重复执行，它会自动做互斥
+# 1.本监控脚本可重复执行，它会自动做自互斥
 # 2.互斥不仅依据监控脚本文件名，而且包含了它的命令行参数，只有整体相同时互斥才生效
 # 3.对于被监控的进程，可以只指定进程名，也可以包含命令行参数
 # 4.不管是监控脚本还是被监控进程，总是只针对属于当前用户下的进程
 #
-# 如果本脚本手工运行正常，但在crontab中运行不正常，则可考虑检查下ps等命令是否可在crontab中正常运行
+# 如果本脚本手工运行正常，但在crontab中运行不正常，
+# 则可考虑检查下ps等命令是否可在crontab中正常运行。
 #
-# 如何监控脚本？
-# 不同环境可能存在差异，需要具体测试，下面是一个示例，但不同环境可能存在差异。
-# 假设有一需要监控脚本：/home/zhangsan/bin/mem.sh
-# 则crontab可配置为：
-# * * * * * /usr/local/bin/process_monitor.sh "mem.sh" "/home/zhangsan/bin/mem.sh &"
+# 注意事项：
+# 不管是监控脚本还是可执行程序，
+# 均要求使用绝对路径，即必须以“/”打头的路径。
 #
-# 下面带路径的用法可能导致监控失效，mem.sh被反复大量拉起，
-# 而监控对象非脚本时，则需要带上路径，这是监控脚本和程序的不同之处：
-# * * * * * /usr/local/bin/process_monitor.sh "/home/zhangsan/bin/mem.sh" "/home/zhangsan/bin/mem.sh &"
-#
-# 当然如果不以“&”的后台方式运行也是可以的，但不推荐这样，因为这阻塞了process_monitor.sh，
-# 实际相当于process_monitor.sh直接在运行。
-#
-# 如果不同路径下，有相同名称的脚本，则需要借助参数进行区分，如：
-# * * * * * /usr/local/bin/process_monitor.sh "mem.sh eth1" "/home/wangwu/bin/mem.sh eth1 &"
-# * * * * * /usr/local/bin/process_monitor.sh "mem.sh eth0" "/home/zhangsan/bin/mem.sh eth0 &"
-#
-# 而程序则可直接路径区分，如（mem为一可执行程序，非脚本文件）：
-# * * * * * /usr/local/bin/process_monitor.sh "/home/wangwu/bin/mem" "/home/wangwu/bin/mem eth1 &"
-# * * * * * /usr/local/bin/process_monitor.sh "/home/zhangsan/bin/mem" "/home/zhangsan/bin/mem eth0 &"
 
 # 实际中，遇到脚本在crontab中运行时，找不到ls和ps等命令
 # 原来是有些环境ls和ps位于/usr/bin目录下，而不是常规的/bin目录
@@ -42,14 +28,18 @@ export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin:$PATH
 trap "" SIGPIPE # 忽略SIGPIPE
 
 # 需要指定个数的命令行参数
-# 参数1：被监控的进程名（可以包含命令行参数）
-# 参数2：重启被监控进程的脚本
+# 参数1：被监控的进程名（可以包含命令行参数，而且必须包含绝对路径方式）
+# 参数2：重启被监控进程的脚本（进程不能以相对路径的方式启动）
 if test $# -ne 2; then
     printf "\033[1;33musage: $0 process_cmdline restart_script\033[m\n"
     printf "\033[1;33mexample: /usr/local/bin/process_monitor.sh \"/usr/sbin/rinetd\" \"/usr/sbin/rinetd\"\033[m\n"
     printf "\033[1;33mplease install process_monitor.sh into crontab by \"* * * * *\"\033[m\n"
     exit 1
 fi
+
+# 设置ONLY_TEST的值为非1关闭测试模式
+# 可设置同名的环境变量ONLY_TEST来控制
+ONLY_TEST=${ONLY_TEST:-0}
 
 process_cmdline="$1" # 需要监控的进程名，或完整的命令行，也可以为部分命令行
 restart_script="$2"  # 用来重启进程的脚本，要求具有可执行权限
@@ -67,11 +57,71 @@ if test $? -ne 0; then
     exit 1
 fi
 
+# 依赖命令ps求进程数
+which ps >/dev/null 2>&1
+if test $? -ne 0; then
+    printf "\033[1;33mcommand 'ps' not exists\033[m\n"
+    exit 1
+fi
+
+# 依赖命令awk求进程数
+which awk >/dev/null 2>&1
+if test $? -ne 0; then
+    printf "\033[1;33mcommand 'awk' not exists\033[m\n"
+    exit 1
+fi
+
+# 依赖命令ls计算日志文件大小
+which ls >/dev/null 2>&1
+if test $? -ne 0; then
+    printf "\033[1;33mcommand 'ls' not exists\033[m\n"
+    exit 1
+fi
+
+# 依赖命令cut计算日志文件大小
+which cut >/dev/null 2>&1
+if test $? -ne 0; then
+    printf "\033[1;33mcommand 'cut' not exists\033[m\n"
+    exit 1
+fi
+
+# 依赖命令tee写日志
+which tee >/dev/null 2>&1
+if test $? -ne 0; then
+    printf "\033[1;33mcommand 'tee' not exists\033[m\n"
+    exit 1
+fi
+
+# 依赖命令mv备份日志文件
+which mv >/dev/null 2>&1
+if test $? -ne 0; then
+    printf "\033[1;33mcommand 'mv' not exists\033[m\n"
+    exit 1
+fi
+
+# 依赖命令sleep
+which sleep >/dev/null 2>&1
+if test $? -ne 0; then
+    printf "\033[1;33mcommand 'sleep' not exists\033[m\n"
+    exit 1
+fi
+
+# 依赖命令sh重启脚本
+which sh >/dev/null 2>&1
+if test $? -ne 0; then
+    printf "\033[1;33mcommand 'sh' not exists\033[m\n"
+    exit 1
+fi
+
 # 下面这段脚本，用来阻止多个监控脚本进程出现
 uid=`id -u $cur_user`
 self_name=`basename $0`
 self_cmdline="$0 $*"
+self_dirpath=$(dirname "$0")
+self_filepath=$self_dirpath/$self_name
 process_name=$(basename `echo "$process_cmdline"|cut -d" " -f1`)
+process_dirpath=$(dirname "$process_cmdline")
+process_filepath=$process_dirpath/$process_name
 process_match="${process_cmdline#* }" # 只保留用来匹配的参数部分
 process_match=$(echo $process_match) # 去掉前后的空格
 
@@ -103,22 +153,75 @@ log()
 
     # 处理日志文件过大
     # 日志加上头[$process_cmdline]，用来区分对不同对象的监控
-    if test ! -z "$file_size"; then        
+    if test ! -z "$file_size"; then
+        LOG_TIME="`date +'%Y-%m-%d %H:%M:%S'`"
+
         if test $file_size -lt $log_filesize; then
-            printf "[$process_cmdline]$record"
-            printf "[$process_cmdline]$record" >> $log_filepath
+            # 不需要滚动
+            if test $ONLY_TEST=1; then
+                printf "[$process_cmdline][$LOG_TIME]$record" |tee -a $log_filepath
+            else
+                printf "[$process_cmdline][$LOG_TIME]$record" >> $log_filepath
+            fi
         else
-            printf "[$process_cmdline]$record" >> $log_filepath
-            mv $log_filepath $log_filepath.bak # 备份
+            # 需要滚动
+            if test $ONLY_TEST=1; then
+                printf "[$process_cmdline][$LOG_TIME]$record" |tee -a $log_filepath
+            else
+                printf "[$process_cmdline][$LOG_TIME]$record" >> $log_filepath
+            fi
 
-            printf "[$process_cmdline][`date +'%Y-%m-%d %H:%M:%S'`]truncated\n"
-            printf "[$process_cmdline][`date +'%Y-%m-%d %H:%M:%S'`]truncated\n" > $log_filepath
+            # 滚动备份
+            mv $log_filepath $log_filepath.bak
 
-            printf "[$process_cmdline]$record"
-            printf "[$process_cmdline]$record" >> $log_filepath            
+            if test $ONLY_TEST=1; then
+                printf "[$process_cmdline][$LOG_TIME]truncated" |tee $log_filepath
+                printf "[$process_cmdline][$LOG_TIME]$record" |tee -a $log_filepath
+            else
+                printf "[$process_cmdline][$LOG_TIME]truncated" > $log_filepath
+                printf "[$process_cmdline][$LOG_TIME]$record" >> $log_filepath
+            fi
         fi
     fi
 }
+
+# 显示调试信息
+if test $ONLY_TEST -eq 1; then
+    log "self_dirpath: $self_dirpath\n"
+    log "self_filepath: $self_filepath\n"
+
+    log "process_name: $process_name\n"
+    log "process_dirpath: $process_dirpath\n"
+    log "process_filepath: $process_filepath\n"
+    log "process_match$process_match\n"
+fi
+
+# 必须使用全路径，即必须以“/”打头
+s1=${self_filepath:0:1}
+p1=${process_filepath:0:1}
+if test $s1 != "/"; then
+    log "illegal, is not an absolute path: $self_cmdline"
+    exit 1
+fi
+if test $p1 != "/"; then
+    log "illegal, is not an absolute path: $process_cmdline"
+    exit 1
+fi
+
+# 取得文件类型
+# process_filetype取值1表示为可执行程序文件
+# process_filetype取值0表示为脚本文件
+file $process_filepath|grep ELF >/dev/null
+if test $? -eq 0; then    
+    process_filetype=1
+else
+    file $process_filepath|grep script >/dev/null
+    if test $? -eq 0; then        
+        process_filetype=0
+    else
+        exit 1
+    fi
+fi
 
 # 命令“ps -C $process_name h -o euid,args”输出示例：
 # 1）目标为非脚本时（process_name值为test）：
@@ -129,7 +232,10 @@ log()
 # 以死循环方式，定时检测指定的进程是否存在
 # 一个重要原因是crontab最高频率为1分钟，不满足秒级的监控要求
 while true; do
-    self_count=`ps -C $self_name h -o euid,args| awk 'BEGIN { num=0; } { if (($1==uid) && match($0, self_cmdline)) {++num;}} END { printf("%d",num); }' uid=$uid cur_user=$cur_user self_cmdline="$self_cmdline"`
+    self_count=`ps -C $self_name h -o euid,args| awk 'BEGIN { num=0; } { if (($1==uid) && ($3==self_filepath) && match($0, self_cmdline)) {++num;}} END { printf("%d",num); }' uid=$uid self_filepath=$self_filepath self_cmdline="$self_cmdline"`
+    if test $ONLY_TEST -eq 1; then
+        log "self_count: $self_count\n"
+    fi
     if test ! -z "$self_count"; then 
         if test $self_count -gt 2; then 
             log "\033[0;32;31m[`date +'%Y-%m-%d %H:%M:%S'`]$0 is running[$self_count/active:$active], current user is $cur_user.\033[m\n"
@@ -144,9 +250,13 @@ while true; do
 
     # 检查被监控的进程是否存在，如果不存在则重启
     if test -z "$process_match"; then
-        process_count=`ps -C $process_name h -o euid,args| awk 'BEGIN { num=0; } { if ($1==uid) {++num;}} END { printf("%d",num); }' uid=$uid cur_user=$cur_user`
+        process_count=`ps -C $process_name h -o euid,args| awk 'BEGIN { num=0; } { if ($1==uid) if ((0==process_filetype) && ($3==process_filepath)) ++num; else if ((1==process_filetype) && ($2==process_filepath)) ++num; } END { printf("%d",num); }' uid=$uid process_filepath=$process_filepath process_filetype=$process_filetype`
     else
-        process_count=`ps -C $process_name h -o euid,args| awk 'BEGIN { num=0; } { if (($1==uid) && match($0, process_match)) {++num;}} END { printf("%d",num); }' uid=$uid cur_user=$cur_user process_match="$process_match"`
+        process_count=`ps -C $process_name h -o euid,args| awk 'BEGIN { num=0; } { if (($1==uid) && match($0, process_match)) if ((0==process_filetype) && ($3==process_filepath)) ++num; else if ((1==process_filetype) && ($2==process_filepath)) ++num; } END { printf("%d",num); }' uid=$uid process_filepath=$process_filepath process_filetype=$process_filetype process_match="$process_match"`
+    fi
+    
+    if test $ONLY_TEST -eq 1; then
+        log "process_count: $process_count\n"
     fi
     if test ! -z "$process_count"; then
         if test $process_count -lt 1; then
