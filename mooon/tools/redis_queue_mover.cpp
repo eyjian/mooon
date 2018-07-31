bffd436e4b3e1329a65e99f6986ec113f29943ae
eyjian <eyjian@live.com>
2018-07-31 17:17:37 +0800

    加入性能统计日志

--- mooon/tools/redis_queue_mover.cpp
+++ mooon/tools/redis_queue_mover.cpp
@@ -19,6 +19,7 @@
  * 搬移redis队列工具，用于将redis一个队列的数据复制到另一队列中，要求为左进右出
  */
 #include <r3c/r3c.h>
+#include <mooon/sys/atomic.h>
 #include <mooon/sys/safe_logger.h>
 #include <mooon/sys/signal_handler.h>
 #include <mooon/sys/thread_engine.h>
@@ -42,7 +43,7 @@ INTEGER_ARG_DEFINE(uint8_t, queues, 1, 1, 10, "the number of queues, e.g. --queu
 // 线程数系数，
 // 注意并不是线程数，线程数为：threads * queues，
 // 假设queues参数值为2，threads参数值为3，则线程数为6
-INTEGER_ARG_DEFINE(uint8_t, threads, 1, 1, 18, "`threads` times `queues` to get number of move threads, e.g., --threads=1");
+INTEGER_ARG_DEFINE(uint8_t, threads, 1, 1, 18, "(threads * queues) to get number of move threads, e.g., --threads=1");
 
 // 源redis
 STRING_ARG_DEFINE(src_redis, "", "the nodes of source redis, e.g., --src_redis=127.0.0.1:6379,127.0.0.1:6380");
@@ -66,16 +67,26 @@ INTEGER_ARG_DEFINE(uint8_t, dst_only_prefix, 0, 0, 1, "the prefix is the key of 
 // 多少个时输出一次计数
 INTEGER_ARG_DEFINE(uint32_t, tick, 10000, 1, 10000000, "the times to tick");
 
+// 统计频率（单位：秒）
+INTEGER_ARG_DEFINE(uint32_t, stat_interval, 2, 1, 86400, "the interval to stat in seconds");
+
 // 轮询队列和重试操作的间隔（单位为毫秒）
-INTEGER_ARG_DEFINE(uint32_t, interval, 100, 1, 1000000, "the interval (milliseconds) to poll or retry");
+INTEGER_ARG_DEFINE(uint32_t, retry_interval, 100, 1, 1000000, "the interval in milliseconds to poll or retry");
 
 // 批量数，即一次批量移动多少
 INTEGER_ARG_DEFINE(int, batch, 1, 1, 100000, "batch to move");
 
 static volatile bool g_stop = false;
+#if __WORDSIZE==64
+static atomic8_t g_num_moved;
+#else
+static atomic_t g_num_moved;
+#endif
+
 static void on_terminated();
-static void signal_thread_proc();
-static void move_thread_proc(uint8_t i);
+static void signal_thread_proc(); // 信号线程
+static void stat_thread_proc(); // 统计线程
+static void move_thread_proc(uint8_t i); // 移动线程
 static std::string get_src_key(uint8_t i);
 static std::string get_dst_key(uint8_t i);
 
@@ -116,8 +127,15 @@ int main(int argc, char* argv[])
 
     try
     {
+#if __WORDSIZE==64
+        atomic8_set(&g_num_moved, 0);
+#else
+        atomic_set(&g_num_moved, 0);
+#endif
+
         mooon::sys::g_logger = mooon::sys::create_safe_logger();
         mooon::sys::CThreadEngine* signal_thread = new mooon::sys::CThreadEngine(mooon::sys::bind(&signal_thread_proc));
+        mooon::sys::CThreadEngine* stat_thread = new mooon::sys::CThreadEngine(mooon::sys::bind(&stat_thread_proc));
 
         const uint8_t num_queues = mooon::argument::queues->value();
         const uint8_t num_threads = num_queues * mooon::argument::threads->value();
@@ -133,6 +151,8 @@ int main(int argc, char* argv[])
         }
         delete []thread_engines;
 
+        stat_thread->join();
+        delete stat_thread;
         signal_thread->join();
         delete signal_thread;
         MYLOG_INFO("mover exit\n");
@@ -158,11 +178,45 @@ void signal_thread_proc()
     }
 }
 
+void stat_thread_proc()
+{
+    try
+    {
+        const uint32_t seconds = mooon::argument::stat_interval->value();
+        uint64_t old_num_moved = 0;
+        uint64_t last_num_moved = 0;
+        mooon::sys::ILogger* stat_logger = mooon::sys::create_safe_logger(true, mooon::SIZE_32, "stat");
+
+        stat_logger->enable_raw_log(true, true);
+        while (!g_stop)
+        {
+            mooon::sys::CUtils::millisleep(seconds * 1000);
+#if __WORDSIZE==64
+            last_num_moved = atomic8_read(&g_num_moved);
+#else
+            last_num_moved = atomic_read(&g_num_moved);
+#endif
+
+            if (last_num_moved > old_num_moved)
+            {
+                const int num_moved = static_cast<int>((last_num_moved - old_num_moved) / seconds);
+                stat_logger->log_raw(" %d/s MOVED\n", num_moved);
+            }
+
+            old_num_moved = last_num_moved;
+        }
+    }
+    catch (mooon::sys::CSyscallException& ex)
+    {
+        MYLOG_ERROR("create stat logger failed: %s\n", ex.str().c_str());
+    }
+}
+
 void move_thread_proc(uint8_t i)
 {
     uint32_t num_moved = 0; // 已移动的数目
     uint32_t old_num_moved = 0; // 上一次移动的数目
-    const uint32_t interval = mooon::argument::interval->value();
+    const uint32_t retry_interval = mooon::argument::retry_interval->value();
     const std::string& src_key = get_src_key(i);
     const std::string& dst_key = get_dst_key(i);
     r3c::CRedisClient src_redis(mooon::argument::src_redis->value());
@@ -195,7 +249,7 @@ void move_thread_proc(uint8_t i)
         }
         if (values.empty())
         {
-            mooon::sys::CUtils::millisleep(interval);
+            mooon::sys::CUtils::millisleep(retry_interval);
             continue;
         }
 
@@ -206,6 +260,12 @@ void move_thread_proc(uint8_t i)
                 dst_redis.lpush(dst_key, values);
 
                 num_moved += static_cast<uint32_t>(values.size());
+#if __WORDSIZE==64
+                atomic8_add(num_moved, &g_num_moved);
+#else
+                atomic_add(num_moved, &g_num_moved);
+#endif
+
                 if (num_moved - old_num_moved >= mooon::argument::tick->value())
                 {
                     old_num_moved = num_moved;
@@ -216,7 +276,7 @@ void move_thread_proc(uint8_t i)
             catch (r3c::CRedisException& ex)
             {
                 MYLOG_ERROR("[%s]=>[%s]: %s\n", src_key.c_str(), dst_key.c_str(), ex.str().c_str());
-                mooon::sys::CUtils::millisleep(interval);
+                mooon::sys::CUtils::millisleep(retry_interval);
             }
         }
     }
